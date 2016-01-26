3592c088a51c8cb7361a0dd0a10e43398b62a21b
KristenGarnier <garnier.kristen@icloud.com>
2016-01-26 19:08:14 +0100

    Playing with real Cycle libraries

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,24 @@
 import Rx from 'rx'
-import Cycle from '@Cycle/core'
+import Cycle from '@cycle/core'
+import {h1, span, makeDOMDriver} from '@cycle/dom'
 
-const h1 = (children) => {
-  return {
-    tagName: 'H1',
-    children
-  }
-}
-
-const span = (children) => {
-  return {
-    tagName: 'SPAN',
-    children
+const style = {
+  h1: {
+    color: 'deepskyblue',
+    fontFamily: 'helvetica',
+    fontWeight: '100'
   }
 }
 
 // Logic (functional)
 function main (DOMSource) {
-  const click$ = DOMSource.DOM.selectEvents('span', 'mouseover')
+  const click$ = DOMSource.DOM.select('span').events('mouseover')
   const sinks = {
     DOM: click$
       .startWith(null)
       .flatMapLatest(() =>
         Rx.Observable.timer(0, 1000)
-         .map(i => h1([
+         .map(i => h1({style: style.h1}, [
            span([
              `Seconds enlapsed ${i}`
            ])
@@ -40,40 +35,13 @@ function main (DOMSource) {
 // sink: output (write) effects
 
 // Effects (imperative)
-function DOMDriver (obj$) {
-  const createElement = (obj) => {
-    const element = document.createElement(obj.tagName)
-    obj.children
-        .filter(c => typeof c === 'object')
-        .map(createElement)
-        .forEach(c => element.appendChild(c))
-    obj.children
-        .filter(c => typeof c === 'string')
-        .forEach(c => element.innerHTML += c)
-    return element
-  }
-
-  obj$.subscribe(obj => {
-    const container = document.querySelector('#app')
-    container.innerHTML = ''
-    const element = createElement(obj)
-    container.appendChild(element)
-  })
-  const DOMSource = {
-    selectEvents (tagName, eventType) {
-      return Rx.Observable.fromEvent(document, eventType)
-        .filter(ev => ev.target.tagName === tagName.toUpperCase())
-    }
-  }
-  return DOMSource
-}
 
 function consoleLogDriver (msg$) {
   msg$.subscribe(msg => console.log(msg))
 }
 
 const drivers = {
-  DOM: DOMDriver,
+  DOM: makeDOMDriver('#app'),
   Log: consoleLogDriver
 }
 
